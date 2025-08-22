ac6b0a2b54cc0dea686f41589bbd28cea9fc4286
Danilo Palen <danilo.palen@uirev.com>
2025-08-22 21:08:12 +1200

    Add title

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   };
   return (
     <html lang="en">
+      <title>Zipang Automotive</title>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <div
