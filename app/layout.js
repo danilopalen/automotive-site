25dbcbdeefe2257b026da4332d9f30e17b485d9e
Danilo Palen <danilo.palen@uirev.com>
2025-08-22 21:03:11 +1200

    Add favicon link

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   };
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <div
           style={{
